56ab4f5094ee7f19a4fdb06b817a5791fb32e499
jeevansanchez <166963895+jeevansanchez@users.noreply.github.com>
2026-01-26 20:18:59 -0500

    some routing work

--- Firmware/testing/upper_board/test.cpp
+++ Firmware/testing/upper_board/test.cpp
@@ -4,6 +4,26 @@
 // test valve control and LED indication
 // make sure espressif board manager URL is added in preferences if uploading from arudino IDE, and make sure in CDC meode. 
 
+#define ADC_MOSI 11
+#define ADC_MISO 13
+#define ADC_CLK 12
+#define ADC_DRDY 14
+#define ADC_CS 0
+
+#include <ADS131M0x.h>
+
+SPIClass SpiADC(HSPI);
+ADS131M0x adc;
+adcOutput adc_out;
+
+void adcInit()
+{
+    adc.setClockSpeed(200000);
+    adc.begin(&SpiADC, ADC_CLK, ADC_MISO, ADC_MOSI, ADC_CS, ADC_DRDY);
+    adc.setInputChannelSelection(0, INPUT_CHANNEL_MUX_AIN0P_AIN0N);
+    adc.setChannelPGA(0, CHANNEL_PGA_1);
+}
+
 const int valveEN1 = 9;
 const int valveEN2 = 3;
 
@@ -43,6 +63,8 @@ void setup() {
         pinMode(leds[i], OUTPUT);
         digitalWrite(leds[i], LOW);
     }
+
+    adcInit();
 }
 
 void loop() {
@@ -55,19 +77,32 @@ void loop() {
                 delay(200);
                 valveControl(valveEN1);
                 break;
-            
             case '2':
                 Serial.println("starting solenoid 2 test");
                 delay(200);
                 valveControl(valveEN2);
                 break;
-
             case '3':
                 Serial.println("starting LED test");
                 delay(200);
                 flashLeds(leds);
                 break;
 
+            case '4':
+                Serial.println("Read ADC data");
+                adc_out = adc.readADC();
+                for(int i = 0; i<4; i++){
+                  if(digitalRead(ADC_DRDY)) Serial.println("ADC Ready");
+                  int32_t output = -1;
+                  if (i == 0) output = adc_out.ch0;
+                  if (i == 1) output = adc_out.ch1;
+                  if (i == 2) output = adc_out.ch2;
+                  if (i == 3) output = adc_out.ch3;
+                  delay(200);
+                  Serial.printf("ADC%d-Val: %d",i, output);
+                  Serial.println("");
+                }
+                break;
             default:
                 Serial.println("uh");
                 break;
