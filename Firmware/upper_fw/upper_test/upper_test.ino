54275a142aa98455f8c9902ea5955c1984dcff5f
Jeevan Sanchez <jeevansanchez42@gmail.com>
2026-02-11 16:50:34 -0500

    remove ai slop

--- Firmware/upper_fw/upper_test/upper_test.ino
+++ Firmware/upper_fw/upper_test/upper_test.ino
@@ -2,7 +2,6 @@
 #include <SPI.h>
 #include <ADS131M0x.h>
 
-/* ===================== ADC PINS ===================== */
 #define ADC_MOSI 11
 #define ADC_MISO 13
 #define ADC_CLK  12
@@ -13,15 +12,12 @@ SPIClass SpiADC(HSPI);
 ADS131M0x adc;
 adcOutput adc_out;
 
-/* ===================== VALVES ===================== */
 const int valveEN1 = 9;
 const int valveEN2 = 3;
 
-/* ===================== LEDS ===================== */
 int leds[] = {35, 36, 37, 48, 40, 41, 42};
 const int ledCount = 7;
 
-/* ===================== ADC INIT ===================== */
 void adcInit() {
     pinMode(ADC_DRDY, INPUT);
 
@@ -30,12 +26,10 @@ void adcInit() {
     adc.setClockSpeed(200000);
     adc.begin(&SpiADC, ADC_CLK, ADC_MISO, ADC_MOSI, ADC_CS, ADC_DRDY);
 
-    // ---- Configure CHANNEL 2 ----
     adc.setInputChannelSelection(2, INPUT_CHANNEL_MUX_AIN0P_AIN0N);
     adc.setChannelPGA(2, CHANNEL_PGA_1);
 }
 
-/* ===================== VALVE CONTROL ===================== */
 void valveControl(const int valvePin) {
     digitalWrite(valvePin, HIGH);
     Serial.println("solenoid ON");
@@ -43,7 +37,6 @@ void valveControl(const int valvePin) {
     digitalWrite(valvePin, LOW);
 }
 
-/* ===================== LED TEST ===================== */
 void flashLeds(const int ledArray[], int count) {
     for (int i = 0; i < count; i++) {
         Serial.print("flashing LED on GPIO ");
@@ -54,11 +47,10 @@ void flashLeds(const int ledArray[], int count) {
     }
 }
 
-/* ===================== SETUP ===================== */
 void setup() {
     Serial.begin(115200);
     Serial.println("start upper-launch-control-board testing");
-    Serial.println("1: solenoid 1 | 2: solenoid 2 | 3: LEDs | 4: ADC CH2");
+    Serial.println("1: run solenoid 1 | 2: run solenoid 2 | 3: run LEDs | 4: ADC test");
 
     pinMode(valveEN1, OUTPUT);
     pinMode(valveEN2, OUTPUT);
@@ -73,7 +65,6 @@ void setup() {
     adcInit();
 }
 
-/* ===================== LOOP ===================== */
 void loop() {
     if (Serial.available() > 0) {
         char input = Serial.read();
@@ -92,19 +83,9 @@ void loop() {
                 break;
 
             case '4':
-                Serial.println("Reading ADC channel 2");
-
-                // DRDY is active-LOW
-                while (digitalRead(ADC_DRDY));
-
-                adc_out = adc.readADC();
-
-                Serial.print("ADC2 Value: ");
-                Serial.println(adc_out.ch2);
-                break;
 
             default:
-                Serial.println("unknown command");
+                Serial.println("uh");
                 break;
         }
     }
